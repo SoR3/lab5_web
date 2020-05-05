e5cc7c6c2780de874002b794937f71b8dc475a55
SoR3 <jystlik@gmail.com>
2020-05-05 09:49:13 +0700

    add templates

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,15 +1,13 @@
-print("Hello world")
 from flask import Flask
 app = Flask(__name__)
 
 #декоратор для вывода страницы по умолчанию
 
-@app.route("/")
+@app.route("/hello")
 def hello():
   return " <html><head></head> <body> Hello World! </body></html>"
 
-if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5000)
+
 
 from flask import render_template
 #наша новая функция сайта
@@ -22,3 +20,6 @@ def data_to():
   #передаем данные в шаблон и вызываем его
   return render_template('simple.html',some_str = some_str,
     some_value = some_value,some_pars=some_pars)
+    
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=5000)
