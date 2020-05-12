6a0ac533af2f7d92d119c15c05ebc1094dd6d881
SoR3 <jystlik@gmail.com>
2020-05-12 22:10:24 +0700

    fix

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -38,16 +38,6 @@ try:
     print(r.text)
 except:
     exit(1)
-    
-    
-try:
-    r = requests.get('http://localhost:5000/data_to')
-    print(r.status_code)
-    if(r.status_code!=200):
-        exit(1)
-    print(r.text)
-except:
-    exit(1)
 
 
 try:
